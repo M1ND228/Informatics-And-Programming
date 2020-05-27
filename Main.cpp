825dc2f62e04e37713ed3cde6d4a721081aba5f5
BrazhnikDA <M1ND228@yandex.ru>
2020-05-27 17:09:10 +0300

    Constructor verification has been implemented

--- Main.cpp
+++ Main.cpp
@@ -37,10 +37,7 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     system("cls");
-    int mass1[100], mass2[100];
-    eDir dir1 = eDir::STOP;
+
     Snake s1;
-    Snake s3(false, 2, 2, 0, 0, mass1, mass2, 0, 16, 10, 0, dir1);
-    Snake s2(s3);
-    mainMenu(s2);
+    mainMenu(s1);
 }
